ab81fed8e8d083782d3b9a4ece1096ef944bf96a
ChellyL <38887991+ChellyL@users.noreply.github.com>
2022-02-23 11:47:48 +0800

    Update bingcntoday.py

--- bingcntoday.py
+++ bingcntoday.py
@@ -3,27 +3,23 @@ import webbrowser
 
 import requests
 
-web = 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN'
+country = 'cn'
+web = 'https://www.bing.com/HPImageArchive.aspx?format=js&n=1&idx=0&cc=' + country
+# web = 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN'
 bing = requests.get(web)
-# print(bing.text)
-
 dicts = bing.json()
-# print(dicts.keys())
-# print(dicts['images'])
-# for k, v in dicts['images'][0].items():
-#   print(str(k) + ' : ' + str(v))
-# print(dicts)
 
-print(dicts['images'][0]['copyright'])
+print(dicts['images'][0]['startdate'] + '\n' + dicts['images'][0]['title'] + '\n' + dicts['images'][0]['copyright'])
 date = dicts['images'][0]['startdate']
-# print('https://www.bing.com'+dicts['images'][0]['url'].strip('&pid=hp'))
-url = 'https://www.bing.com' + dicts['images'][0]['url'].strip('&pid=hp')
+# url = 'https://www.bing.com' + dicts['images'][0]['url'].strip('&pid=hp')
+urlbase = 'https://www.bing.com' + dicts['images'][0]['urlbase']
+url = urlbase + '_1920x1080.jpg'
+
 paper = requests.get(url)
 describe = dicts['images'][0]['urlbase'].strip('/th?id=OHR.')
-# print(describe)
+
 num = re.compile(r'\d*')
 simple = num.sub('', describe)
-# print(simple)
 name = date + '-' + simple + '.jpg'
 image = open(name, 'wb')
 # webbrowser.open(url)
